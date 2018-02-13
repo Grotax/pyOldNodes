613ca8ef05ff2166fe68b6d83f1027527e4df8db
Benjamin Brahmer <info@b-brahmer.de>
2018-02-13 10:35:29 +0100

    better display

--- parse.py
+++ parse.py
@@ -1,18 +1,37 @@
 #!/usr/bin/python3
+"""Durchsucht die lokale nodes.json auf alte Knoten mit einer Firmware aus 2016"""
 
 import json
+try:
+    from terminaltables import AsciiTable
+    USE_TABLE = True
+except ImportError:
+    USE_TABLE = False
 
 def main():
+    """the main function"""
     with open('nodes.json') as file:
         data = json.load(file)
         nodes = data['nodes']
+        table_data = [['Name', 'Kontakt', 'Firmware']]
         for i in nodes:
             if data['nodes'][i]["nodeinfo"]["software"]["firmware"]["release"].startswith("2016"):
                 try:
                     owner = data['nodes'][i]['nodeinfo']['owner']['contact']
                 except KeyError:
                     owner = "N/A"
-                print("%s: %s" % (data['nodes'][i]['nodeinfo']['hostname'], owner))
+                table_data.append(
+                    [data['nodes'][i]['nodeinfo']['hostname'],
+                     owner,
+                     data['nodes'][i]["nodeinfo"]["software"]["firmware"]["release"]]
+                    )
+        if USE_TABLE:
+            table = AsciiTable(table_data)
+            print(table.table)
+        else:
+            for i in table_data:
+                print(i)
+            print("für bessere Tabellen: pip install terminaltables")
 
 
 if __name__ == '__main__':
