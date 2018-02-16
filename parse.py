5cbfc9ff8a1e0690859d16f86604c77e31fc8971
Talon <talontalon24@gmail.com>
2018-02-16 21:51:49 +0100

    Including Argparse and writing a HTML file

--- parse.py
+++ parse.py
@@ -2,6 +2,19 @@
 """Durchsucht die lokale nodes.json auf alte Knoten mit einer Firmware aus 2016"""
 
 import json
+import argparse
+
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument("-j" "--json", metavar="Path", dest="json_path",
+                    help="Path to the nodes.json file", required=True)
+PARSER.add_argument("--html", metavar="Path", dest="html_path", default=False,
+                    help="Path where to store HTML output")
+
+#ja pfad zur nodes.json erzwungen und
+#optional html für eine noch zu implementierende html ausgabe
+
+ARGS = PARSER.parse_args()
+
 
 try:
     from terminaltables import AsciiTable
@@ -11,7 +24,7 @@ except ImportError:
 
 def main():
     """the main function"""
-    with open('nodes.json') as file:
+    with open(ARGS.json_path) as file:
         data = json.load(file)
     try:
         nodes = data['nodes']
@@ -47,6 +60,18 @@ def main():
     if USE_TABLE:
         table = AsciiTable(table_data)
         print(table.table)
+        if ARGS.html_path:
+            with open(ARGS.html_path, "w") as file:
+                html_content = ""
+                html_row = ""
+                for row in table_data:
+                    html_cell = ""
+                    for cell in row:
+                        html_cell += "<th>{}</th>".format(cell)
+                    html_row = "<tr>{}</tr>".format(html_cell)
+                    html_content += html_row
+                html_table = '<table border=1 align="center">{}</table>'.format(html_content)
+                file.write(html_table)
     else:
         maxlengths = [max([len(row[col]) for row in table_data]) for col in range(3)]
         for row in table_data:
